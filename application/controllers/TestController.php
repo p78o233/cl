93389b639eacaaee81f7bf07296ab5c7ff88ca06
吴翰文 <wuhanwen@bluemoon.com.cn>
2020-01-18 22:47:58 +0800

    毫秒测试

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -41,4 +41,19 @@ class TestController extends CI_Controller
 //		JSON_UNESCAPED_UNICODE  中文不转义
 		echo json_encode($result,JSON_UNESCAPED_UNICODE);
 	}
+//	从1加到一亿
+	public function add(){
+		$sum = 0;
+		$time = microtime(true);
+		$begin = (int)($time * 1000);
+		for($i = 1 ; $i <= 100000000 ; $i++){
+			$sum+=$i;
+		}
+		$time = microtime(true);
+		$end = (int)($time * 1000);
+		echo $end - $begin ;
+//		$t1 = microtime(true);
+//		$t2 = microtime(true);
+//		echo (($t2-$t1)*1000);
+	}
 }
